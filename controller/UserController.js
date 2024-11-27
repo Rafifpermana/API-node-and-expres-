34628ea3cd7a811e19908d9833a79a660df2a0f2
DRIV32 <rafifpermana39@gmail.com>
2024-11-27 16:48:11 +0700

    add status type

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,22 +1,55 @@
 import User from "../model/UserModel.js";
 
+const checkUser = async (req, res) => {
+  const existingUser = await User.findOne({ where: { NPM } });
+  if (existingUser) {
+    return res.status(409).json({ message: "Conflict: User already exists" });
+  }
+};
+
 export const getUser = async (req, res) => {
+  const { NPM } = req.params;
+
   try {
-    const response = await User.findAll({
-      attributes: ["NPM", "nama", "email", "gender", "alamat", "password"],
-    });
-    res.status(200).json(response);
+    const user = await User.findOne({ where: { NPM } });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ message: "Not Found: User does not exist" });
+    }
+    res.status(200).json(user);
   } catch (error) {
-    console.log(error.message);
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error: Error retrieving user", error });
   }
 };
 
 export const createUser = async (req, res) => {
+  if (!NPM || !nama || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Bad Request: Missing required fields" });
+  }
+
   try {
-    const response = await User.create(req.body);
-    res.status(201).json(response);
+    const newUser = await User.create({
+      NPM,
+      nama,
+      email,
+      gender,
+      alamat,
+      password,
+    });
+    res
+      .status(201)
+      .json({ message: "User  created successfully", user: newUser });
   } catch (error) {
-    console.log(error.message);
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Internal Server Error: Error creating user", error });
   }
 };
 
--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 import authRoute from "./routes/authRoute.js";
 import db from "./config/database.js";
+import errorHandler from "./middleware.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 app.use(authRoute);
+app.use(errorHandler);
 
 app.listen(5000, () => console.log("Server Running..."));
